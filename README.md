1bbdaa87bbd4c4be3062c6b2754cec1a310e96bb
uddip3815 <gogoim045@gmail.com>
2020-11-28 13:22:21 +0530

    first commit

---
+++ README.md
@@ -0,0 +1,12 @@
+# W-Gen
+# Installation
+* `apt-get update -y`
+* `apt-get upgrade -y`
+* `apt-get install python -y`
+* `apt-get install git -y`
+* `git clone https://github.com/uddip3815/W-Gen.git`
+* `cd $HOME`
+* `ls`
+* `cd W-Gen`
+* `ls`
+* `python lazybee.py`
---
+++ wgen.py
@@ -0,0 +1,52 @@
+print('''
+            WW                    WW            GG            EEEEEEEE      NN     NNNN
+             WW        WW        WW           GG              EE            NN    NN NN
+              WW     WW  WW     WW          GG    GGGGGG      EEEEE         NN   NN  NN
+               WW   WW    WW   WW            GG   GG  GG      EE            NN  NN   NN
+                WWWW       WWWW               GGGG     GG     EEEEEEEE      NNNN     NN     
+                                                                                            v 1.0
+''')
+print ('''
+                        00 00000    00     00       
+                        00     00     00  00        0   0  0 0000  00000  0000000
+                        00 000000      0000         0   0  0    0    0    00    0
+                        00    000       00          0   0  0    0    0    0000000
+                        00 00000        00          00000  0 0000  00000  00
+''')
+print (" ")
+print (" ")
+
+length=int(input("Enter the number of characters: "))
+print (" ")
+li_name=input("Name your wordlist: ")
+print (" ")
+print ("Wordlist Generating Please Wait!")
+print (" ")
+lista=[0 for x in range(length)]
+x=length-1
+string="abcdefghijklmnopqrstuvwxyz1234567890!@#$%^&*"
+list_of_results=[]
+file1=open(li_name,"w")
+while(x>-1):
+    result=""
+    if lista[x]==len(string)-1:
+        for z in range (length):
+            result+=string[lista[z]]
+        lista[x]=0
+        x-=1
+    elif x==length-1:
+        for z in range(length):
+            result+=string[lista[z]]
+        lista[x]+=1
+    else:
+        for z in range(length):
+            result+=string[lista[z]]
+        lista[x]+=1
+        if x>0:
+            x+=1
+        else:
+            x=length-1
+    file1.write(result+"\n")
+print ("Completed Generating Wordlist")
+print (" ")
+print ("Please check "+str(li_name)+" in your W-Gen directoy")
